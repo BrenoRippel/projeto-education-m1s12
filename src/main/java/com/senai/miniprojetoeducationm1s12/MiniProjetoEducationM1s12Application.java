34458332805133eefed46b28c4ac6dac8c7d559a
Breno Rippel <39842293+BrenoRippel@users.noreply.github.com>
2024-04-02 22:54:46 -0300

    first commit

---
+++ src/main/java/com/senai/miniprojetoeducationm1s12/MiniProjetoEducationM1s12Application.java
@@ -0,0 +1,13 @@
+package com.senai.miniprojetoeducationm1s12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetoEducationM1s12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjetoEducationM1s12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/senai/miniprojetoeducationm1s12/MiniProjetoEducationM1s12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.miniprojetoeducationm1s12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetoEducationM1s12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
